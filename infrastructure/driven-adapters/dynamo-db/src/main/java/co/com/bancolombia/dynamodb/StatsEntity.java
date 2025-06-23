d443570869b570073368b78dd99eea97a2c71b20
Dani-Rua <danigodmontoya@gmail.com>
2025-06-22 22:48:24 -0500

    feat(dynamodb): implementar adaptador para persistir estadísticas en DynamoDB

---
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/StatsEntity.java
@@ -0,0 +1,116 @@
+package co.com.bancolombia.dynamodb;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamoDbBean
+public class StatsEntity {
+
+    private String timestamp;
+    private int totalContactoClientes;
+    private int motivoReclamo;
+    private int motivoGarantia;
+    private int motivoDuda;
+    private int motivoCompra;
+    private int motivoFelicitaciones;
+    private int motivoCambio;
+    private String hash;
+
+//    public StatsEntity() {
+//    }
+//
+//    public StatsEntity(LocalDateTime timestamp, int totalContactoClientes, int motivoReclamo, int motivoGarantia, int motivoDuda, int motivoCompra, int motivoFelicitaciones, int motivoCambio, String hash) {
+//        this.timestamp = timestamp;
+//        this.totalContactoClientes = totalContactoClientes;
+//        this.motivoReclamo = motivoReclamo;
+//        this.motivoGarantia = motivoGarantia;
+//        this.motivoDuda = motivoDuda;
+//        this.motivoCompra = motivoCompra;
+//        this.motivoFelicitaciones = motivoFelicitaciones;
+//        this.motivoCambio = motivoCambio;
+//        this.hash = hash;
+//    }
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("timestamp")
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+//    public void setTimestamp(LocalDateTime timestamp) {
+//        this.timestamp = timestamp;
+//    }
+//
+//    public int getTotalContactoClientes() {
+//        return totalContactoClientes;
+//    }
+//
+//    public void setTotalContactoClientes(int totalContactoClientes) {
+//        this.totalContactoClientes = totalContactoClientes;
+//    }
+//
+//    public int getMotivoReclamo() {
+//        return motivoReclamo;
+//    }
+//
+//    public void setMotivoReclamo(int motivoReclamo) {
+//        this.motivoReclamo = motivoReclamo;
+//    }
+//
+//    public int getMotivoGarantia() {
+//        return motivoGarantia;
+//    }
+//
+//    public void setMotivoGarantia(int motivoGarantia) {
+//        this.motivoGarantia = motivoGarantia;
+//    }
+//
+//    public int getMotivoDuda() {
+//        return motivoDuda;
+//    }
+//
+//    public void setMotivoDuda(int motivoDuda) {
+//        this.motivoDuda = motivoDuda;
+//    }
+//
+//    public int getMotivoCompra() {
+//        return motivoCompra;
+//    }
+//
+//    public void setMotivoCompra(int motivoCompra) {
+//        this.motivoCompra = motivoCompra;
+//    }
+//
+//    public int getMotivoFelicitaciones() {
+//        return motivoFelicitaciones;
+//    }
+//
+//    public void setMotivoFelicitaciones(int motivoFelicitaciones) {
+//        this.motivoFelicitaciones = motivoFelicitaciones;
+//    }
+//
+//    public int getMotivoCambio() {
+//        return motivoCambio;
+//    }
+//
+//    public void setMotivoCambio(int motivoCambio) {
+//        this.motivoCambio = motivoCambio;
+//    }
+//
+//    public String getHash() {
+//        return hash;
+//    }
+//
+//    public void setHash(String hash) {
+//        this.hash = hash;
+//    }
+}
